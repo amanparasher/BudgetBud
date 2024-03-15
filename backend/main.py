ffe991f7367b1511f1349bc9805e59c6ccaba3c9
Annarhysa Albert <annarhysa13@gmail.com>
2024-03-15 20:15:29 +0530

    working on ml

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import numpy as np
+import os
 
-data = pd.read_csv()
+# Specify the path of the directory you want to open
+directory_path = 'backend/data'
+
+# Change the current working directory to the specified directory
+os.chdir(directory_path)
+
+data = pd.read_csv("expense.csv")
+print(data)
