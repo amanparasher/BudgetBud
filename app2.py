d44a9a57e23d394bd1b35edeb5f7548eba72336c
Sreyaaas <sm7183@srmist.edu.in>
2024-03-16 00:38:01 +0530

    Many Changed

--- app2.py
+++ app2.py
@@ -1,10 +1,10 @@
-from flask import Flask, request ,render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('hero.html')
+    return app.send_static_file('wow.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
