a0a659691d9636ed02d1f681216c7b9c0044f25a
Prithvi2310 <shahprithvi1505@gmail.com>
2024-03-16 11:59:44 +0530

    added a bunch more stuff in analysis.py

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+pd.set_option('display.max_rows',None)
 
 def remove_comma(string):
     return string.replace(',','')
@@ -9,11 +10,20 @@ def find_month(string):
     month = int(string.split('/')[1])
     return month_dict[month]
 
-data = pd.read_csv('../../spending.csv')
+data = pd.read_csv('spending.csv')
 data['description'] = data['description'].fillna('misc')
 
 data['amount'] = data['amount'].map(remove_comma).copy()
 data['amount'] = data['amount'].astype(int)
 
+#finding the spending by month 
 data['month'] = data['date'].apply(find_month)
 data_by_month = data.groupby('month',sort=False)['amount'].sum() * -1
+
+#finding the spending by category
+data_by_category = data.groupby(['category'])['amount'].sum() * -1
+data_by_category = data_by_category[data_by_category>0]
+
+#data for monthly category 
+data_monthly_category = data.set_index(['month','category']).copy()
+data_monthly_category = data_monthly_category.groupby(level=[0,1],sort=False)['amount'].sum() * -1
